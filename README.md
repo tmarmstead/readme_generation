8e09bd977d9a669779d8ddc279abcd2059afc273
Titina Armstead <71151032+tmarmstead@users.noreply.github.com>
2021-09-13 06:27:38 -0700

    updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# README.md Generator 
+# Employee Directory 
 
   ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+
+  ![Screenshot (210)](https://user-images.githubusercontent.com/71151032/133091463-1dc04b02-524d-4bb1-96c4-38a712a69ed5.png)
+  ![Screenshot (211)](https://user-images.githubusercontent.com/71151032/133091715-27636ad0-d890-4c87-ba89-1b817d5eeb5a.png)
+
 
   ## Table of Contents: 
   - [Description](#Description)
@@ -13,20 +18,18 @@
   - [Questions](#Questions)
   
   ## Description: 
-  Use the command line which will take in user input to create a polished and properly formatted README.md file
+  This app allows users to view and entire employee directory for quick access to employee information
  
   ## Demo:
   ![Untitled_ Jan 21, 2021 12_00 PM](https://user-images.githubusercontent.com/71151032/105384636-78a7d900-5be0-11eb-8962-17ba723098cd.gif)
 
-  Link to video walkthrough: 
-  
-  https://drive.google.com/file/d/1xH-42FhAGogtRVaiMP0IeE_bHc9r7bKA/view
+  [Link to video walkthrough](https://drive.google.com/file/d/1xH-42FhAGogtRVaiMP0IeE_bHc9r7bKA/view)
 
   ## Installation: 
-  Clone https code to repository using `git clone` https://github.com/tmarmstead/readme_generation.git
+  To install, copy the code form the repo and use "git clone <paste copied information here>" to add the app to a directory
   
   ## Usage: 
-  This application helps developers save time and brainpower by efficiently creating a well formatted README.md file for any project
+  This app is for employees or managers that want to view non-sensitive date about the employees in their company. I would be particulary helpful to be able to filter employees by name.
   
   ## License: 
   Click the link below for full licensing information: 
@@ -37,10 +40,10 @@
  
   
   ## Contribution: 
-  Interested in Contributing? Contribution is welcomed in the following ways and more: contacting tina.armstead@gmail.com, fixing errors, suggesting features, submitting pull requests
+  Contribution welcomed!
   
   ## Test: 
-  Testing 1, 2, 3
+  
   
   ## Questions: 
   If you have any questions you can reach me with the below contact information: 
--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown');
 const path = require('path');
 
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the Title?',
-        name: 'title',
-      },
-      {
-        type: 'input',
-        message: 'What is the Description?',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'What are the Installition instructions?',
-        name: 'installation',
-      },
-      {
-          type: 'input',
-          message: 'What is the usage of this application?',
-          name: 'usage',
-        },
-      {
-        type: 'list',
-        message: 'Select a license:',
-        name: 'license',
-        choices: ['MIT', 'IPL', 'Apache 2.0', 'BSD 3', 'None']
-      },
-      {
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'github',
-      },
-      {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-      },
-      {
-          type: 'input',
-          message: 'What is the Contributing information?',
-          name: 'contribution',
-        },
-      {
-        type: 'input',
-        message: 'What is the Test information?',
-        name: 'test',
-      },
-        
-    ]
+  {
+    type: 'input',
+    message: 'What is the Title?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'What is the Description?',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What are the Installition instructions?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'What is the usage of this application?',
+    name: 'usage',
+  },
+  {
+    type: 'list',
+    message: 'Select a license:',
+    name: 'license',
+    choices: ['MIT', 'IPL', 'Apache 2.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the Contributing information?',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'What is the Test information?',
+    name: 'test',
+  },
 
-// TODO: Create a function to write README file
+]
+
+// Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFileSync(path.join(__dirname, fileName), data);
+  fs.writeFileSync(path.join(__dirname, fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+  inquirer.prompt(questions)
     .then((data) => {
-    writeToFile('README.md', generateMarkdown(data)); 
+      writeToFile('README.md', generateMarkdown(data));
     });
 }
 
